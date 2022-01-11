95293b5b4d2da89e4621690fef83440afae35567
James Diel <jamesin4d@users.noreply.github.com>
2022-01-11 06:32:47 -0600

    Delete behavior_tree directory

--- behavior_tree/nodes/bt_node.gd
+++
@@ -1,8 +0,0 @@
-extends BehaviorTree
-
-class_name BehaviorTreeNode, "../icons/action.png"
-
-enum { SUCCESS, FAILURE, RUNNING }
-
-func tick(node, board):
-	pass
--- behavior_tree/nodes/composites/composite.gd
+++
@@ -1,9 +0,0 @@
-extends BehaviorTreeNode
-
-class_name Composite, "../../icons/category_composite.png"
-
-func _ready():
-	if self.get_child_count() < 1:
-		print("BehaviorTree Error: Composite %s should have at least one child" % self.name)
-
-# DO NOT CHANGE THIS SCRIPT. GO TO INSPECTOR SCRIPT -> EXTEND SCRIPT
